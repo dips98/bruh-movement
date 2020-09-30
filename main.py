9021887d70548ff1e36b380854beac33200ae119
dips98 <62205185+dips98@users.noreply.github.com>
2020-10-01 03:42:19 +0530

    Update main.py
    Just removed extra bracket for readability of code
    #bruh moment 
    add Minecraft mp3 too

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import random
 from playsound import playsound
 
 while True:
-    if(random.randint(0,100000000)<100):
+    if random.randint(0,100000000) < 100:
         playsound("Bruh Sound Effect #2.mp3")
